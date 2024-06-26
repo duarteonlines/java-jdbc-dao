97609feede86fbdb16e343c21b9eef8764cddee7
Pedro Oliveira <p.duarteoliveiraa@gmail.com>
2024-06-26 10:24:37 -0300

    criando entidade classe seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
+
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller sl = new Seller(2, "Pedro", "pedro@gmail.com", LocalDate.now(), 3000.0, obj);
+        System.out.println(sl);
     }
 }
