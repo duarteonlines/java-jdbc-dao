cd64bc15225e5208db2e07233911e8cb443affdf
Pedro Oliveira <p.duarteoliveiraa@gmail.com>
2024-06-26 10:36:55 -0300

    criando sellerdaojdbc e daofactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,7 @@ public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
         Seller sl = new Seller(2, "Pedro", "pedro@gmail.com", LocalDate.now(), 3000.0, obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(sl);
     }
 }
