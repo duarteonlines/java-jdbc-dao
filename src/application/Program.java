42475b524822c8324af681f74d290591c5e54bf0
Pedro Oliveira <p.duarteoliveiraa@gmail.com>
2024-06-26 10:58:26 -0300

    implementando busca por id

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,15 +4,13 @@ import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
-
-import java.time.LocalDate;
+import java.util.Date;
 
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-        Seller sl = new Seller(2, "Pedro", "pedro@gmail.com", LocalDate.now(), 3000.0, obj);
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        System.out.println(sl);
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
     }
 }
